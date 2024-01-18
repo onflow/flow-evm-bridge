c434e247bde657a4e7ad4909ee6271b15293e5b8
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2024-01-18 15:10:06 -0600

    restructure project directories

---
+++ cadence/scripts/bridge/token_uri.cdc
@@ -0,0 +1,18 @@
+import "EVM"
+
+import "FlowEVMBridgeUtils"
+
+access(all) fun main(coaHost: Address, tokenID: UInt256, contractAddressHex: String): String {
+    let coa: &EVM.BridgedAccount = getAuthAccount<auth(BorrowValue) &Account>(coaHost).storage.borrow<&EVM.BridgedAccount>(
+            from: /storage/evm
+        )!
+    let calldata: [UInt8] = FlowEVMBridgeUtils.encodeABIWithSignature("tokenURI(uint256)", [tokenID])
+    let response: [UInt8] = coa.call(
+            to: FlowEVMBridgeUtils.getEVMAddressFromHexString(address: contractAddressHex)!,
+            data: calldata,
+            gasLimit: 15000000,
+            value: EVM.Balance(flow: 0.0)
+        )
+    let decodedResponse: [AnyStruct] = EVM.decodeABI(types: [Type<String>()], data: response)
+    return decodedResponse[0] as! String
+}
--- pass_precompiles.sh
+++ pass_precompiles.sh
@@ -3,76 +3,76 @@
 # We need to get past the 20 sequential pre-compile COA addresses in this emulator version
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
 
 flow evm create-account 0.0
-flow transactions send ./transactions/evm/destroy_coa.cdc
+flow transactions send ./cadence/transactions/evm/destroy_coa.cdc
