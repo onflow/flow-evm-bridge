d80c34968b56f2a3036c12f293dc527a91f8803c
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2024-02-02 14:44:49 -0600

    add example ERC721 contract for EVM-native NFT path poc

--- setup.sh
+++ setup.sh
@@ -19,9 +19,6 @@ flow transactions send ./cadence/transactions/evm/deploy.cdc --args-json "$(cat 
 flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeUtils.cdc 9ca416871ee388c7a41e0b7886dfcc47e08bbdef
 flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeTemplates.cdc
 
-# Fund the Utils contract COA
-flow evm fund 000000000000000000000000000000000000001b 100.0
-
 # Deploy main bridge contract
 flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridge.cdc
 
@@ -56,3 +53,12 @@ flow transactions send ./cadence/transactions/evm/deposit.cdc 10.0 --signer erc7
 flow accounts add-contract ./cadence/contracts/example-assets/ExampleNFT.cdc --signer example-nft
 flow transactions send ./cadence/transactions/example-assets/setup_collection.cdc --signer user
 flow transactions send ./cadence/transactions/example-assets/mint_nft.cdc f3fcd2c1a78f5eee example description thumbnail '[]' '[]' '[]' --signer example-nft
+
+# Deploy ExampleERC721 contract with erc721's COA as owner
+flow transactions send ./cadence/transactions/evm/deploy.cdc --args-json "$(cat deploy-erc721-args.json)" --signer erc721
+
+# Mint an ERC721 to the user's COA
+flow transactions send ./cadence/transactions/evm/call.cdc \
+    d69e40309a188ee9007da49c1cec5602d7f9d767 \
+    cd279c7c000000000000000000000000000000000000000000000000000000000000001b000000000000000000000000000000000000000000000000000000000000002a0000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000003b62616679626569676479727a74357366703775646d37687537367568377932366e6633656675796c71616266336f636c67747179353566627a64690000000000 \
+    12000000 0.0 --signer erc721
---
+++ solidity/src/ExampleERC721.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ExampleERC721 is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    constructor() ERC721("NAME", "SYMBOL") Ownable(msg.sender) {}
+
+    function safeMint(address to, uint256 tokenId, string memory uri) public onlyOwner {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
