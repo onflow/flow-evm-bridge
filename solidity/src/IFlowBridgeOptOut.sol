7bb29a7d6ecad6002161c3b07676eb4c4e08953e
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2024-03-08 18:53:46 -0600

    update evm bridge opt-out mechanism

--- solidity/src/IFlowBridgeOptOut.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-
-interface IFlowBridgeOptOut is IERC165 {
-    function confirmOptOut() external view returns (bool);
-}
