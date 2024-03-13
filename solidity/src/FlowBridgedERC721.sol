5bb7c0f6cfda834b82b05254f5f99ba83e402e61
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2024-03-13 15:15:10 -0500

    add ERC721Enumerable conformance to FlowBridgedERC721.sol

--- solidity/src/FlowBridgedERC721.sol
+++ solidity/src/FlowBridgedERC721.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// TODO: Implement extensions ERC721Upgradable, Metadata, Enumerable, URIStorage, Royalty etc.
-contract FlowBridgedERC721 is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract FlowBridgedERC721 is ERC721, ERC721URIStorage, ERC721Burnable, ERC721Enumerable, Ownable {
     string public flowNFTAddress;
     string public flowNFTIdentifier;
     string public contractMetadata;
@@ -30,16 +30,8 @@ contract FlowBridgedERC721 is ERC721, ERC721URIStorage, ERC721Burnable, Ownable 
         _setTokenURI(tokenId, uri);
     }
 
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
-    }
-
     function contractURI() public view returns (string memory) {
         return contractMetadata;
-    }
-
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
-        return super.supportsInterface(interfaceId);
     }
 
     function getFlowNFTAddress() public view returns (string memory) {
@@ -50,7 +42,32 @@ contract FlowBridgedERC721 is ERC721, ERC721URIStorage, ERC721Burnable, Ownable 
         return flowNFTIdentifier;
     }
 
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
     function exists(uint256 tokenId) public view returns (bool) {
         return _ownerOf(tokenId) != address(0);
+    }
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
     }
 }
