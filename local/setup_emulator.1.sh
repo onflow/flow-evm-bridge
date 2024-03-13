6fd44b30918cbe094c3beb9bb8bbbb0404ae4eb1
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2024-03-13 18:36:05 -0500

    add setup instructions to README

--- local/setup_emulator.1.sh
+++ local/setup_emulator.1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+flow-c1 transactions send ./cadence/transactions/evm/create_account.cdc 100.0
+
 flow-c1 accounts add-contract ./cadence/contracts/utils/ArrayUtils.cdc
 flow-c1 accounts add-contract ./cadence/contracts/utils/StringUtils.cdc
 flow-c1 accounts add-contract ./cadence/contracts/utils/ScopedFTProviders.cdc
@@ -7,7 +9,6 @@ flow-c1 accounts add-contract ./cadence/contracts/utils/ScopedFTProviders.cdc
 flow-c1 accounts update-contract ./cadence/contracts/standards/EVM.cdc
 
 # Create COA in emulator-account
-flow-c1 transactions send ./cadence/transactions/evm/create_account.cdc 100.0
 
 # Deploy the Factory contract - NOTE THE `deployedContractAddress` IN THE EMITTED EVENT
 flow-c1 transactions send ./cadence/transactions/evm/deploy.cdc \
