650d6eecc884b3c139420ba7955bb6342511db3e
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2024-03-18 17:33:09 -0500

    add Cadence tests to CI workflow

---
+++ local/normalize_coverage_report.sh
@@ -0,0 +1,3 @@
+sed -i 's/A.0000000000000007.SerializationInterfaces/cadence\contracts\/SerializationInterfaces.cdc/' coverage.lcov
+sed -i 's/A.0000000000000007.Serialize/cadence\/contracts\/utils\/Serialize.cdc/' coverage.lcov
+sed -i 's/A.0000000000000007.SerializeNFT/cadence\contracts\/SerializeNFT.cdc/' coverage.lcov
