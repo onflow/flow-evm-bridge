a14ede17a84e6883bb90a4b0b2ed66d394a547d3
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2024-03-19 18:19:59 -0500

    minimize serialization utils by removing interfaces & strategies

--- local/normalize_coverage_report.sh
+++ local/normalize_coverage_report.sh
@@ -1,3 +1,2 @@
-sed -i 's/A.0000000000000007.SerializationInterfaces/cadence\contracts\/SerializationInterfaces.cdc/' coverage.lcov
 sed -i 's/A.0000000000000007.Serialize/cadence\/contracts\/utils\/Serialize.cdc/' coverage.lcov
 sed -i 's/A.0000000000000007.SerializeNFT/cadence\contracts\/SerializeNFT.cdc/' coverage.lcov
