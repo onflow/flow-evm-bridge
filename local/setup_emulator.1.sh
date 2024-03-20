4ab4e7b576e58d07d4667bd03e96472bc17c09bb
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2024-03-20 17:33:31 -0500

    update setup scripts

--- local/setup_emulator.1.sh
+++ local/setup_emulator.1.sh
@@ -10,12 +10,28 @@ flow accounts update-contract ./cadence/contracts/standards/EVM.cdc
 
 # Create COA in emulator-account
 
-# Deploy the Factory contract - NOTE THE `deployedContractAddress` IN THE EMITTED EVENT
-flow transactions send ./cadence/transactions/evm/deploy.cdc \
-    --args-json "$(cat ./cadence/args/deploy-factory-args.json)"
-
 # Deploy initial bridge contracts
 flow accounts add-contract ./cadence/contracts/bridge/BridgePermissions.cdc
 flow accounts add-contract ./cadence/contracts/bridge/ICrossVM.cdc
 flow accounts add-contract ./cadence/contracts/bridge/CrossVMNFT.cdc
 flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeConfig.cdc
+
+
+flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeUtils.cdc \
+    --args-json "$(cat ./cadence/args/deploy-factory-args.json)"
+
+flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeNFTEscrow.cdc
+flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeTemplates.cdc
+# Add the templated contract code chunks for FlowEVMBridgedNFTTemplate.cdc contents
+flow transactions send ./cadence/transactions/bridge/admin/upsert_contract_code_chunks.cdc \
+    --args-json "$(cat ./cadence/args/bridged-nft-code-chunks-args.json)" --gas-limit 1600
+
+flow accounts add-contract ./cadence/contracts/bridge/IEVMBridgeNFTMinter.cdc
+
+# Deploy Serialization Utils
+flow accounts add-contract ./cadence/contracts/utils/Serialize.cdc
+flow accounts add-contract ./cadence/contracts/utils/SerializeNFT.cdc
+
+# Deploy main bridge interface & contract
+flow accounts add-contract ./cadence/contracts/bridge/IFlowEVMNFTBridge.cdc
+flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridge.cdc
