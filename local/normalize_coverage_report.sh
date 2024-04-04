ec71c6c1ca6722b5a9f7607d4edee3ce7d21f18e
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2024-04-03 22:57:01 -0500

    add new contracts to coverage normalization script

--- local/normalize_coverage_report.sh
+++ local/normalize_coverage_report.sh
@@ -3,9 +3,15 @@ sed -i 's/A.0000000000000007.SerializeNFT/cadence\contracts\/SerializeNFT.cdc/' 
 sed -i 's/A.0000000000000007.BridgePermissions/cadence\/contracts\/bridge\/BridgePermissions.cdc/' coverage.lcov
 sed -i 's/A.0000000000000007.ICrossVM/cadence\/contracts\/bridge\/ICrossVM.cdc/' coverage.lcov
 sed -i 's/A.0000000000000007.CrossVMNFT/cadence\/contracts\/bridge\/CrossVMNFT.cdc/' coverage.lcov
+sed -i 's/A.0000000000000007.CrossVMToken/cadence\/contracts\/bridge\/CrossVMToken.cdc/' coverage.lcov
 sed -i 's/A.0000000000000007.FlowEVMBridgeConfig/cadence\/contracts\/bridge\/FlowEVMBridgeConfig.cdc/' coverage.lcov
 sed -i 's/A.0000000000000007.FlowEVMBridgeUtils/cadence\/contracts\/bridge\/FlowEVMBridgeUtils.cdc/' coverage.lcov
+sed -i 's/A.0000000000000007.FlowEVMBridgeNFTEscrow/cadence\/contracts\/bridge\/FlowEVMBridgeNFTEscrow.cdc/' coverage.lcov
+sed -i 's/A.0000000000000007.FlowEVMBridgeTokenEscrow/cadence\/contracts\/bridge\/FlowEVMBridgeTokenEscrow.cdc/' coverage.lcov
 sed -i 's/A.0000000000000007.FlowEVMBridgeTemplates/cadence\/contracts\/bridge\/FlowEVMBridgeTemplates.cdc/' coverage.lcov
-sed -i 's/A.0000000000000007.IFlowEVMNFTBridge/cadence\/contracts\/bridge\/FlowEVMBridge.cdc/' coverage.lcov
+sed -i 's/A.0000000000000007.IEVMBridgeNFTMinter/cadence\/contracts\/bridge\/IEVMBridgeNFTMinter.cdc/' coverage.lcov
+sed -i 's/A.0000000000000007.IEVMBridgeTokenMinter/cadence\/contracts\/bridge\/IEVMBridgeTokenMinter.cdc/' coverage.lcov
+sed -i 's/A.0000000000000007.IFlowEVMNFTBridge/cadence\/contracts\/bridge\/IFlowEVMNFTBridge.cdc/' coverage.lcov
+sed -i 's/A.0000000000000007.IFlowEVMTokenBridge/cadence\/contracts\/bridge\/IFlowEVMTokenBridge.cdc/' coverage.lcov
 sed -i 's/A.0000000000000007.FlowEVMBridge/cadence\/contracts\/bridge\/FlowEVMBridge.cdc/' coverage.lcov
 sed -i 's/A.0000000000000007.EVMBridgeRouter/cadence\/contracts\/bridge\/EVMBridgeRouter.cdc/' coverage.lcov
