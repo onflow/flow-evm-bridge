2833c879d40b1dfc57c1bb5bcc21245eb9b22e9f
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2024-04-05 15:22:25 -0500

    update flow command in setup scripts

--- local/setup_emulator.1.sh
+++ local/setup_emulator.1.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 # Create COA in emulator-account
-flow transactions send ./cadence/transactions/evm/create_account.cdc 100.0
+flow-c1 transactions send ./cadence/transactions/evm/create_account.cdc 100.0
 
 # Deploy supporting utils
-flow accounts add-contract ./cadence/contracts/utils/ArrayUtils.cdc
-flow accounts add-contract ./cadence/contracts/utils/StringUtils.cdc
-flow accounts add-contract ./cadence/contracts/utils/ScopedFTProviders.cdc
-flow accounts add-contract ./cadence/contracts/utils/Serialize.cdc
-flow accounts add-contract ./cadence/contracts/utils/SerializeNFT.cdc
+flow-c1 accounts add-contract ./cadence/contracts/utils/ArrayUtils.cdc
+flow-c1 accounts add-contract ./cadence/contracts/utils/StringUtils.cdc
+flow-c1 accounts add-contract ./cadence/contracts/utils/ScopedFTProviders.cdc
+flow-c1 accounts add-contract ./cadence/contracts/utils/Serialize.cdc
+flow-c1 accounts add-contract ./cadence/contracts/utils/SerializeNFT.cdc
 
-flow accounts update-contract ./cadence/contracts/standards/EVM.cdc
+flow-c1 accounts update-contract ./cadence/contracts/standards/EVM.cdc
 
 # Deploy initial bridge contracts
-flow accounts add-contract ./cadence/contracts/bridge/BridgePermissions.cdc
-flow accounts add-contract ./cadence/contracts/bridge/ICrossVM.cdc
-flow accounts add-contract ./cadence/contracts/bridge/CrossVMNFT.cdc
-flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeConfig.cdc
+flow-c1 accounts add-contract ./cadence/contracts/bridge/BridgePermissions.cdc
+flow-c1 accounts add-contract ./cadence/contracts/bridge/ICrossVM.cdc
+flow-c1 accounts add-contract ./cadence/contracts/bridge/CrossVMNFT.cdc
+flow-c1 accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeConfig.cdc
 
 # Deploy FlowEVMBridgeUtils also deploying FlowEVMBridgeFactory to EVM in init
-flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeUtils.cdc \
+flow-c1 accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeUtils.cdc \
     --args-json "$(cat ./cadence/args/deploy-factory-args.json)"
 
-flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeNFTEscrow.cdc
-flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeTemplates.cdc
+flow-c1 accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeNFTEscrow.cdc
+flow-c1 accounts add-contract ./cadence/contracts/bridge/FlowEVMBridgeTemplates.cdc
 
 # Add the templated contract code chunks for FlowEVMBridgedNFTTemplate.cdc contents
-flow transactions send ./cadence/transactions/bridge/admin/upsert_contract_code_chunks.cdc \
+flow-c1 transactions send ./cadence/transactions/bridge/admin/upsert_contract_code_chunks.cdc \
     --args-json "$(cat ./cadence/args/bridged-nft-code-chunks-args.json)" --gas-limit 1600
 
-flow accounts add-contract ./cadence/contracts/bridge/IEVMBridgeNFTMinter.cdc
+flow-c1 accounts add-contract ./cadence/contracts/bridge/IEVMBridgeNFTMinter.cdc
 
 # Deploy main bridge interface & contract
-flow accounts add-contract ./cadence/contracts/bridge/IFlowEVMNFTBridge.cdc
-flow accounts add-contract ./cadence/contracts/bridge/FlowEVMBridge.cdc
+flow-c1 accounts add-contract ./cadence/contracts/bridge/IFlowEVMNFTBridge.cdc
+flow-c1 accounts add-contract ./cadence/contracts/bridge/FlowEVMBridge.cdc
