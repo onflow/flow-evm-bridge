c41711981752b6a5788ce9a61de3321d10e6f059
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2024-05-06 16:25:12 -0500

    remove unused test import

--- solidity/test/FlowBridgeFactory.t.sol
+++ solidity/test/FlowBridgeFactory.t.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.17;
 
 import {Test} from "forge-std/Test.sol";
-import {console} from "forge-std/console.sol";
 
 import {FlowBridgeDeploymentRegistry} from "../src/FlowBridgeDeploymentRegistry.sol";
 import {FlowEVMBridgedERC721Deployer} from "../src/FlowEVMBridgedERC721Deployer.sol";
