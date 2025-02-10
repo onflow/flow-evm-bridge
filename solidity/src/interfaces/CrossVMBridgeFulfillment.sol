dfcd8d975c4a512e9cdd295a7e7bc35d37849ed4
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2025-02-10 16:40:37 -0700

    add pre & post fulfillment hooks to CrossVMBridgeFulfillment

--- solidity/src/interfaces/CrossVMBridgeFulfillment.sol
+++ solidity/src/interfaces/CrossVMBridgeFulfillment.sol
@@ -4,9 +4,20 @@ pragma solidity 0.8.24;
 import {CrossVMBridgeCallable} from "./CrossVMBridgeCallable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+/**
+ * @dev Related to https://github.com/onflow/flips/issues/318[FLIP-318] Cross VM NFT implementations
+ * on Flow in the context of Cadence-native NFTs. The following base contract must be implemented to
+ * integrate with the Flow VM bridge connecting Cadence & EVM implementations so that the canonical
+ * VM bridge may move the Cadence NFT into EVM in a mint/escrow pattern.
+ */
 abstract contract CrossVMBridgeFulfillment is CrossVMBridgeCallable, ERC721 {
 
+    // Encountered when attempting to fulfill a token that has been previously minted and is not
+    // escrowed in EVM under the VM bridge
     error FulfillmentFailedTokenNotEscrowed(uint256 id, address escrowAddress);
+
+    // Emitted when an NFT is moved from Cadence into EVM
+    event FulfilledToEVM(address indexed recipient, uint256 indexed tokenId);
 
     /**
      * Initializes the bridge EVM address such that only the bridge COA can call privileged methods
@@ -26,6 +37,7 @@ abstract contract CrossVMBridgeFulfillment is CrossVMBridgeCallable, ERC721 {
      *      bridging into EVM
      */
     function fulfillToEVM(address _to, uint256 _id, bytes memory _data) external onlyVMBridge {
+        _beforeFulfillment(_to, _id, _data); // hook allowing implementation to perform pre-fulfillment validation
         if (_ownerOf(_id) == address(0)) {
             _mint(_to, _id); // Doesn't exist, mint the token
         } else {
@@ -33,13 +45,26 @@ abstract contract CrossVMBridgeFulfillment is CrossVMBridgeCallable, ERC721 {
             _requireEscrowed(_id);
             safeTransferFrom(vmBridgeAddress(), _to, _id);
         }
+        _afterFulfillment(_to, _id, _data); // hook allowing implementation to perform pre-fulfillment processing
+        emit FulfilledToEVM(_to, _id);
+    }
+
+    /**
+     * @dev Returns whether the token is currently escrowed under custody of the designated VM bridge
+     * 
+     * @param _id the ID of the token in question
+     */
+    function isEscrowed(uint256 _id) public view returns (bool) {
+        return _ownerOf(_id) == vmBridgeAddress();
     }
 
     /**
      * @dev Allows a caller to determine the contract conforms to the `ICrossVMFulfillment` interface
      */
-    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
-        return true; // tmp
+    function supportsInterface(bytes4 interfaceId) public view virtual override(CrossVMBridgeCallable, ERC721) returns (bool) {
+        return interfaceId == type(CrossVMBridgeFulfillment).interfaceId
+            || interfaceId == type(CrossVMBridgeCallable).interfaceId
+            || super.supportsInterface(interfaceId);
     }
 
     /**
@@ -49,10 +74,35 @@ abstract contract CrossVMBridgeFulfillment is CrossVMBridgeCallable, ERC721 {
      * @param _id the token id that must be escrowed
      */
     function _requireEscrowed(uint256 _id) internal view {
-        address owner = _ownerOf(_id);
-        address vmBridgeAddress_ = vmBridgeAddress();
-        if (owner != vmBridgeAddress_) {
-            revert FulfillmentFailedTokenNotEscrowed(_id, vmBridgeAddress_);
+        if (!isEscrowed(_id)) {
+            revert FulfillmentFailedTokenNotEscrowed(_id, vmBridgeAddress());
         }
+    }
+
+    /**
+     * @dev This internal method is included as a step implementations can override and have
+     * executed in the default fullfillToEVM call.
+     * 
+     * @param _to address of the pending token recipient
+     * @param _id the id of the token to be moved into EVM from Cadence
+     * @param _data any encoded metadata passed by the corresponding Cadence NFT at the time of
+     *      bridging into EVM
+     */
+    function _beforeFulfillment(address _to, uint256 _id, bytes memory _data) internal virtual {
+        // No-op by default, meant to be overridden by implementations
+    }
+
+    /**
+     * @dev This internal method is included as a step implementations can override and have
+     * executed in the default fullfillToEVM call.
+     * 
+     * @param _to address of the pending token recipient
+     * @param _id the id of the token to be moved into EVM from Cadence
+     * @param _data any encoded metadata passed by the corresponding Cadence NFT at the time of
+     *      bridging into EVM
+     */
+    function _afterFulfillment(address _to, uint256 _id, bytes memory _data) internal virtual {
+        // No-op by default, meant to be overridden by implementations for things like processing
+        // and setting metadata
     }
 }
